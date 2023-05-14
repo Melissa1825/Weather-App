bf935d12e2c3b5c8aaa3a3c71b460068d14aa376
Melissa1825 <114811994+Melissa1825@users.noreply.github.com>
2023-05-13 20:16:57 -0400

    Updated HTML, CSS & JS with another project being worked on

--- js/main.js
+++ js/main.js
@@ -1,84 +1,83 @@
-//Temp display function
-function displayTemp(response) {
-    //console.log(response.data);
-    let city = document.querySelector('#cityName').innerHTML = response.data.name;
-    let temp = document.querySelector('#currentTemp').innerHTML = Math.round(response.data.main.temp);
-    let desc = document.querySelector('#description').innerHTML = response.data.weather[0].main;
-    let feel = document.querySelector('#feelsLike').innerHTML = Math.round(response.data.main.feels_like);
-    let wind = document.querySelector('#wind').innerHTML = response.data.wind.speed;
-    let humid = document.querySelector('#humid').innerHTML = response.data.main.humidity;
-    let date = document.querySelector('#currentDate').innerHTML = formatDate(response.data.dt * 1000);
-    //*******setup icon for brokend img!!!********
-    let icon = document.querySelector('#icon').setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+function search(city) {
+    let apiKey = '9acca644b3a6c9504b178f06o3c4t156'
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
 
-    celTemp = response.data.main.temp;
+    axios.get(apiUrl).then(showTemp)
 }
 
-//******WHEN MINUTES END IN 0 TIME NOT FORMATTING PROPERLY******* */
-//Date formatting
-function formatDate(timestamp) {
+search ('Toronto')
+
+
+document.querySelector('#citySearch').addEventListener('submit', handleSearch)
+
+function handleSearch(event) {
+    event.preventDefault();
+    let cityName = document.querySelector('#cityInput')
+    search(cityName.value)
+}
+
+
+function showTemp(response) {
+console.log(response)
+document.querySelector('#currentCity').innerHTML = response.data.city;
+
+celTemp = response.data.temperature.current;
+document.querySelector('#currentTemp').innerHTML = Math.round(celTemp);
+
+document.querySelector('#description').innerHTML = response.data.condition.description;
+document.querySelector('#feelsLike').innerHTML = Math.round(response.data.temperature.feels_like);
+document.querySelector('#windSpeed').innerHTML = Math.round(response.data.wind.speed);
+document.querySelector('#humid').innerHTML = response.data.temperature.humidity;
+document.querySelector('#icon').setAttribute('src', `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+document.querySelector('#icon').setAttribute('alt', response.data.condition.description);
+document.querySelector('#date').innerHTML = formatDate(response.data.time * 1000)
+
+
+}
+
+
+function formatDate(timestamp){
     let date = new Date(timestamp);
 
-    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let day = days[date.getDay()];
+    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let weekday = days[date.getDay()];
 
     let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    let month = months[date.getMonth()]
+    let month = months[date.getMonth()];
 
-    let dayNum = date.getDate()
+    let day = date.getDate();
 
-    let hours = date.getHours();
+    //time not right for different time zones
+    let hours = date.getHours()
     if (hours < 10) {
         hours = `0${hours}`
     }
-    let min = date.getMinutes();
+
+    let min = date.getMinutes()
     if (min < 10) {
-        minutes = `0${min}`;
+        min = `0${min}`
     }
-
-    return `${day}, ${month} ${dayNum} ${hours}:${min}`
-}
-
-
-//Default city on load
-function search(city) {
-    let apiKey = "a0776b8bb0b1556bee1e5658c3acf0c4"
-    //let city = 'london'
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-
-    axios.get(apiUrl).then(displayTemp)
-}
-
-search('Toronto')
-
-
-//Search Engine
-document.querySelector('#searchForm').addEventListener('submit', handleSubmit);
-
-function handleSubmit(event) {
-    event.preventDefault();
-    let citySearch = document.querySelector('#cityInput').value
-    search(citySearch)
-    //console.log(citySearch)
-}
-
-//Unit Conversion Cel to Far
-//Cel to Far
-document.querySelector('#far').addEventListener('click', convertToFar)
-
-function convertToFar(event) {
-    event.preventDefault();
-    let farTemp = (celTemp * 9) / 5 + 32;
-    document.querySelector('#currentTemp').innerHTML = Math.round(farTemp);
+    
+    return `${weekday}, ${month} ${day} ${hours}:${min}`
 }
 
 let celTemp = null;
 
-//Far to Cel
-let celLink = document.querySelector('#cel').addEventListener('click', convertToCel);
+document.querySelector('#far').addEventListener('click', convertToFar);
+
+function convertToFar(event) {
+    event.preventDefault();
+    let temp = document.querySelector('#currentTemp');
+    let farTemp = (celTemp * 9/5) + 32;
+    temp.innerHTML = Math.round(farTemp);
+}
+
+document.querySelector('#cel').addEventListener('click', convertToCel);
 
 function convertToCel(event) {
     event.preventDefault();
-    document.querySelector('#currentTemp').innerHTML = Math.round(celTemp);
+    let temp = document.querySelector('#currentTemp');
+    temp.innerHTML = Math.round(celTemp)
 }
+
 
